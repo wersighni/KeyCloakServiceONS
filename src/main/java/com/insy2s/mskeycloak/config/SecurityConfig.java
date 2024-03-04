f524ef92ee527413f8ea6feef79c23769b8cc10a
peter.mollet <pmollet@insy2s.fr>
2024-03-04 14:51:08 +0100

    [FEAT][PM] - added spring.cloud.config.profile in .yml files. Added .mvn. Removed CORS, not necessary anymore since it's being done in the gateway

--- src/main/java/com/insy2s/mskeycloak/config/SecurityConfig.java
+++ src/main/java/com/insy2s/mskeycloak/config/SecurityConfig.java
@@ -3,11 +3,8 @@ package com.insy2s.mskeycloak.config;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,11 +13,6 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 /**
  * SecurityConfig.
@@ -47,9 +39,6 @@ public class SecurityConfig {
             "/api/mail"
     };
 
-    @Value("${allowed.origins}")
-    private String[] allowedOrigins;
-
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
@@ -64,7 +53,6 @@ public class SecurityConfig {
     protected DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .authorizeHttpRequests(
                         authorizeRequests -> authorizeRequests
@@ -79,34 +67,6 @@ public class SecurityConfig {
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        log.info("Allowing origins: {}", String.join(",", this.allowedOrigins));
-        CorsConfiguration corsConfigurationSource = new CorsConfiguration();
-        corsConfigurationSource.setAllowedOrigins(List.of(allowedOrigins));
-        corsConfigurationSource.setAllowedHeaders(List.of(
-                HttpHeaders.AUTHORIZATION,
-                HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.ACCEPT,
-                "x-auth-token"
-        ));
-        corsConfigurationSource.setAllowedMethods(
-                List.of(
-                        HttpMethod.GET.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.PATCH.name(),
-                        HttpMethod.DELETE.name(),
-                        HttpMethod.OPTIONS.name()
-                )
-        );
-        corsConfigurationSource.setAllowCredentials(true);
-        corsConfigurationSource.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfigurationSource);
-        return source;
     }
 
 }
