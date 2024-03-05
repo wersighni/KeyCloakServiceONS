81f5996933cbd918a535990b428549aeed06f0b1
peter.mollet <pmollet@insy2s.fr>
2024-03-05 12:18:24 +0100

    [FIX][PM] - added surefire in pom.xml.

--- src/main/java/com/insy2s/mskeycloak/client/IMailClient.java
+++ src/main/java/com/insy2s/mskeycloak/client/IMailClient.java
@@ -3,14 +3,13 @@ package com.insy2s.mskeycloak.client;
 import com.insy2s.mskeycloak.dto.MailDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "EmailService", url = "${email.url}")
+@FeignClient(value = "email-service")
 public interface IMailClient {
 
-    @PostMapping("/")
+    @PostMapping("/api/mail/")
     ResponseEntity<Boolean> sendEmail(@RequestBody MailDto mailDto);
 
 }
