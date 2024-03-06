0b90d3988e70977e9165113e5e5f68729c807c50
Hedia <selmi.hedia@gmail.com>
2024-03-06 09:58:33 +0100

    maj

--- src/main/java/com/insy2s/mskeycloak/client/IMailClient.java
+++ src/main/java/com/insy2s/mskeycloak/client/IMailClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "EmailService", url = "${email.url}")
+@FeignClient(value = "Email-Service", url = "${email.url}")
 public interface IMailClient {
 
     @PostMapping("/")
