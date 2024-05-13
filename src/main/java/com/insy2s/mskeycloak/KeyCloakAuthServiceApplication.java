f13dd28bc373a45c1783bd3f39550501e2f60e16
Hedia <selmi.hedia@gmail.com>
2024-05-13 13:19:21 +0100

    suivi projet  access

--- src/main/java/com/insy2s/mskeycloak/KeyCloakAuthServiceApplication.java
+++ src/main/java/com/insy2s/mskeycloak/KeyCloakAuthServiceApplication.java
@@ -108,6 +108,9 @@ public class KeyCloakAuthServiceApplication {
         AccessDto menuProjet = saveAccess("Projet", "Projet", "Menu", null, null, new ArrayList<>());
         AccessDto listProjet = saveAccess("Liste des projets", "lstProjets", "Page", "projets", accessMapper.toEntity(menuProjet), new ArrayList<>());
         saveAccess("Ajouter un projet", "createprojet", "Page", "createprojet", accessMapper.toEntity(menuProjet), new ArrayList<>());
+
+        saveAccess("Suivre un projet", "suivi", "Page", "suivi", accessMapper.toEntity(menuProjet), new ArrayList<>());
+
         saveAccess("Suivre un projet", "suiviProject", "Action", null, accessMapper.toEntity(listProjet), new ArrayList<>());
         saveAccess("Modifier un Projet", "updateProject", "Action", null, accessMapper.toEntity(listProjet), new ArrayList<>());
         saveAccess("Ajouter un Projet", "addProject", "Action", null, accessMapper.toEntity(listProjet), new ArrayList<>());
