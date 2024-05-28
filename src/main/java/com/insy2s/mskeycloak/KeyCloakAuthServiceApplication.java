9bf1213697aaaa70a910422820fc576fa9ca1e5d
annabiInsy2s <mannabi@insy2s.fr>
2024-05-28 13:55:53 +0100

    first commit

--- src/main/java/com/insy2s/mskeycloak/KeyCloakAuthServiceApplication.java
+++ src/main/java/com/insy2s/mskeycloak/KeyCloakAuthServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @Slf4j
-@EnableDiscoveryClient
 @SpringBootApplication
 @RequiredArgsConstructor
 @EnableFeignClients
